dae0e82b1dfbc0c92d719a4f15fef8fdab0872c1
Shining Ying <shiningdesign@live.com>
2017-01-03 16:28:05 +0800

    console or consoless mode launcher (20170102)

--- universal_tool_template.bat
+++ universal_tool_template.bat
@@ -1,3 +1,11 @@
 @echo off
+REM YourPythonFileName_w.bat will launch without console
+REM YourPythonFileName.bat will launch with console
+set file=%~n0
+if "%file:~-2%" equ "_w" (
+  start pythonw %~dp0%file:~0,-2%.py
+  goto done
+)
 call python %~dp0%~n0.py
 pause
+:done
