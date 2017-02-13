3c3ffe0101229048172a29e9b618eb2081e70b7c
Shining Ying <shiningdesign@live.com>
2017-02-13 11:49:00 +0800

    Update universal_tool_template.bat
    update for python 2 and 3 subfix auto launch

--- universal_tool_template.bat
+++ universal_tool_template.bat
@@ -1,11 +1,26 @@
 @echo off
+set p3=D:\z_sys\App_Dev\Python35x64_01\
+
 REM YourPythonFileName_w.bat will launch without console
 REM YourPythonFileName.bat will launch with console
+REM YourPythonFileName_z.bat will launch without console in Python3
+REM YourPythonFileName_x.bat will launch with console in Python3
+
 set file=%~n0
 if "%file:~-2%" equ "_w" (
   start pythonw %~dp0%file:~0,-2%.py
   goto done
 )
+if "%file:~-2%" equ "_z" (
+  start %p3%pythonw.exe %~dp0%file:~0,-2%.py
+  goto done
+)
+if "%file:~-2%" equ "_x" (
+  %p3%python.exe %~dp0%file:~0,-2%.py
+  goto console
+)
 call python %~dp0%~n0.py
+
+:console
 pause
 :done
