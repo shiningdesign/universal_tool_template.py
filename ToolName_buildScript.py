6dbfe40f84d2c3148e2a3b923506440173672352
Shining Ying <shiningdesign@live.com>
2018-09-24 18:50:31 +0800

    fix icon option to make the buildScript work

--- ToolName_buildScript.py
+++ ToolName_buildScript.py
@@ -2,10 +2,10 @@ from cx_Freeze import setup, Executable
 import sys
 
 base = 'Win32GUI' if sys.platform=='win32' else None
-buildOptions = dict(packages = [], excludes = [], includes = ["atexit"], include_files = ["icons/"], icon="ToolName.ico")
+buildOptions = dict(packages = [], excludes = [], includes = ["atexit"], include_files = ["icons/"])
 
 executables = [
-    Executable('ToolName.py', base=base)
+    Executable('ToolName.py', base=base, icon="icons/ToolName.ico")
 ]
 setup(
     name='ToolName',
