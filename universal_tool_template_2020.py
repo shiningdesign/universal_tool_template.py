b677dd89e3df198bcdb70c125342bdc8d4d45c02
Shining Ying Xiong <shiningdesign@live.com>
2022-08-07 13:55:40 +0800

    add unity

--- universal_tool_template_2020.py
+++ universal_tool_template_2020.py
@@ -1,6 +1,6 @@
 # Univeral Tool Template v20.2
 tpl_ver = 20.20
-tpl_date = 220503
+tpl_date = 220807
 print("tpl_ver: {0}-{1}".format(tpl_ver, tpl_date))
 # by ying - https://github.com/shiningdesign/universal_tool_template.py
 
